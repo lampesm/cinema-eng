8429e4cf758992b10edad43a1a5207639fa517f3
esmail211 <smaeil0018@gmail.com>
2022-04-23 16:49:53 +0430

    add bootstrap and update template css

--- config/settings.py
+++ config/settings.py
@@ -128,6 +128,11 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'boot'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
